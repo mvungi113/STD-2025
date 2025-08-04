7cfb678f976403425511068faa1fe6660dabd4c8
rogasian <rogasianmvungi@gmail.com>
2025-08-04 16:20:35 +0300

    javascript intro

--- learn-js/script.js
+++ learn-js/script.js
@@ -1,3 +1,47 @@
-console.log("Hello, World!");
+// console.log("Hello, World!");
 
-alert("Welcome to JavaScript Learning!");
+// alert("Welcome to JavaScript Learning!");
+
+// js comments
+/* 
+multiline comments
+This is a multiline comment.
+*/
+
+//varable -- variables are used to store data/information
+
+
+let message;
+
+message = "Hello, JavaScript!";
+console.log(message);
+
+let firstname = "John",
+    lastname= "Mbinda",
+    age=5;
+console.log(firstname, lastname, age);
+
+var city = "New York";
+console.log(city);
+
+let a ="hello Jumanji djhf ghhgs ";
+let b = a;
+console.log(b);
+
+// let, var, const
+
+const pi = 3.14;
+console.log(pi);
+
+//pi = 32;
+console.log(pi); // This will throw an error because pi is a constant and cannot be reassigned
+
+
+const BGCOLOR = "blue";
+const TEXTCOLOR = "ffffff"
+
+
+console.log(BGCOLOR, TEXTCOLOR);
+
+
+// Data types in JavaScript

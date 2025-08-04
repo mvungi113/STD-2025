feb0b4021998aa9a70fbc988db8ae670745c0514
rogasian <rogasianmvungi@gmail.com>
2025-08-04 16:20:44 +0300

    javascript intro

--- learn-js/script.js
+++ learn-js/script.js
@@ -19,6 +19,7 @@ console.log(message);
 let firstname = "John",
     lastname= "Mbinda",
     age=5;
+    console.log(typeof age)
 console.log(firstname, lastname, age);
 
 var city = "New York";
@@ -45,3 +46,21 @@ console.log(BGCOLOR, TEXTCOLOR);
 
 
 // Data types in JavaScript
+
+let number = 42; // Number
+console.log(typeof number); // Output: number
+number= "42"; // String
+console.log(typeof number); // Output: string
+
+console.log(1/0); // Output: Infinity
+console.log(-1/0); // Output: -Infinity 
+console.log(0/0); // Output: NaN (Not a Number)
+console.log("4 / 2 = " + 4/2); // Output: 2
+
+let st ="Hello, World!";
+console.log(st); // Output: Hello, World!
+
+let isNumber=  true; // Boolean
+console.log(isNumber); // Output: true
+let isLogin = false; // Boolean
+console.log(isLogin); // Output: false

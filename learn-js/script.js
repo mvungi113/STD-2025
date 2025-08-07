48a1107440872aaeaa97ebb5d1d0b2312cd0996a
rogasian <rogasianmvungi@gmail.com>
2025-08-07 14:32:07 +0300

    continue with js

--- learn-js/script.js
+++ learn-js/script.js
@@ -64,3 +64,97 @@ let isNumber=  true; // Boolean
 console.log(isNumber); // Output: true
 let isLogin = false; // Boolean
 console.log(isLogin); // Output: false
+
+// alert("This is an alert message!"); // Alert box
+
+//prompt
+
+//promt user input
+// let result = prompt("Enter your name: "); // Prompt box
+// alert("Welcome, " + result + "!"); // Output the entered name
+
+
+//confirm
+
+// let isBoss = confirm("Are you the boss?"); // Confirmation box
+// console.log(isBoss)
+
+
+//  type conversion 
+// let value = false;
+// alert(typeof value); // Output: boolean
+
+// // /string conversion
+// value = String(value);
+// alert(typeof value); // Output: string
+
+// value = Number(value);
+// alert(typeof value); // Output: number
+
+// alert(Boolean(1))
+// alert(Boolean("hello"))
+
+
+let x, y,z;
+x=100;
+y=200;
+
+///addition`````""
+
+console.log(`Sum ${x} + ${y} = ${x + y}`); // Output: 300
+console.log("Sum " + x + " + " + y + " = " + (x + y)); // Output: 300
+
+sum = x + y;
+console.log("Sum: = " + sum); // Output: Sum: 300
+console.log(`Sum: = ${sum}`); // Output: Sum: 300
+
+// Substraction
+sub = y - x;
+console.log("Substraction: = " + sub); // Output: Substraction: 100
+
+// multiplication
+mul = x * y;
+console.log("Multiplication: = " + mul); // Output: Multiplication: 200
+
+//division
+div = y / x;    
+console.log("Division: = " + div); // Output: Division: 2
+
+// modulus
+mod = y % x;            
+console.log("Modulus: = " + mod); // Output: Modulus: 0
+
+let ax,bx,cx;
+ax = 10;
+// ax = bx = cx = 10;
+bx = cx = ax; // Assigning the value of ax to bx and cx
+
+console.log("ax: " + ax); // Output: 10
+console.log("bx: " + bx); // Output: 10     
+console.log("cx: " + cx); // Output: 10
+
+
+let n = 2;
+n = n + 5;
+n = n * 2;
+console.log("n: " + n); // Output: 14
+
+
+// comparison 
+
+console.log('a' > 'b'); // Output: false
+console.log('a' < 'b'); // Output: true
+console.log('a' === 'b'); // Output: false
+console.log('a' !== 'b'); // Output: true
+
+console.log(1 > 2);
+console.log(1 < 2);
+console.log(1 == 2);
+console.log(1 != 2);
+console.log(1 >= 2);
+console.log(1 <= 2);
+
+
+
+
+console.log(" " == false)
